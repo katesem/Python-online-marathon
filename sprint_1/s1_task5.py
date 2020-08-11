6d1f824a8e2f46a51197ef02d9dcf1ff3013985b
katesem <50136680+katesem@users.noreply.github.com>
2020-08-11 16:34:11 +0300

    Update s1_task5.py

--- sprint_1/s1_task5.py
+++ sprint_1/s1_task5.py
@@ -1,26 +1,25 @@
 #solution of this task based on Stack data structure and operations with it
 
 def toPostFixExpression(e):
-    prec = {}      # stores operator priorities, where 3 - the highest priority, 1 - the lowest
+    prec = {}                    # stores operator priorities, where 3 - the highest priority, 1 - the lowest
     prec["*"] = 3  
     prec["/"] = 3
     prec["+"] = 2
     prec["-"] = 2
     prec["("] = 1
     
-    stack = [] # for keeping operators.
-    output = []   # list for output.
+    stack = []        # for keeping operators.
+    output = []       # list for output.
     
     for el in e:
         if el.isdigit():
-            output.append(el)    # appending  to the end of the output list.
-        
+            output.append(el)           # appending  to the end of the output list.
         
         elif el  == '(':
             stack.insert(0,el)
             
         elif el == ')':
-            top_element = stack[0] # imitation of Stack method pop()
+            top_element = stack[0]           # imitation of Stack method pop()
             del stack[0]
             
             while top_element != '(':
@@ -29,8 +28,7 @@ def toPostFixExpression(e):
                 del stack[0]
                 
         else:
-
-            while len(stack) > 0  and prec[stack[0]] >= prec[el]:  # if stack is not empty and priority of first stack element higher
+            while len(stack) > 0  and prec[stack[0]] >= prec[el]:           # if stack is not empty and priority of first stack element higher
                 output.append(stack[0])
                 del stack[0]
             stack.insert(0,el)
