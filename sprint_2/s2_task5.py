3a20544bc1e00fdd97a9bc976bd0ad944212a1dd
katesem <50136680+katesem@users.noreply.github.com>
2020-08-14 21:24:53 +0300

    Solution of task 5
    Input data : list of strings with information about some location.
    Using regular expression was written method max_population() for parsing strings and get info about location with highest population .

---
+++ sprint_2/s2_task5.py
@@ -0,0 +1,17 @@
+import re
+
+def max_population(s):
+    r = int(max([re.sub(r"[,_a-z]", "", i[4:]) for i in s]))
+    return (tuple([i for i in s if str(r) in i][0].split(','))[1],r)
+    
+    
+#Test Example :   
+print(max_population(["id,name,poppulation,is_capital",
+"3024,eu_kyiv,24834,y",
+"3025,eu_volynia,20231,n",
+"3026,eu_galych,23745,n",
+"4892,me_medina,18038,n",
+"4401,af_cairo,18946,y",
+"4700,me_tabriz,13421,n",
+"4899,me_bagdad,22723,y",
+"6600,af_zulu,09720,n"]))
