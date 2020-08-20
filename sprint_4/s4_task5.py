367514d6b8599c4adba9643a1f8231f012aadbc2
katesem <50136680+katesem@users.noreply.github.com>
2020-08-20 12:47:19 +0300

    Solution of task 5

--- sprint_4/s4_task5.py
+++ sprint_4/s4_task5.py
@@ -1,1 +1,18 @@
-
+class Gallows:
+    
+    def __init__(self, words = [], game_over = False):
+        self.words = words
+        self.game_over = game_over
+        
+    def play(self, word):      
+        if not self.words or (word not in self.words and self.words[-1][-1] == word[0]):
+            self.words.append(word)
+            return self.words
+        else:
+            self.game_over = True
+            return "game over" 
+    
+    def restart(self):
+        self.words = []
+        self.game_over = False
+        return "game restarted"
